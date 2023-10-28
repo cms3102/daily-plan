04e2758a193bf825e7781cc0a29d2d5a30fc5d8d
CMS <ayd07@naver.com>
2023-10-28 22:40:29 +0900

    set dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.sergio.dailyplan"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.sergio.dailyplan"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
