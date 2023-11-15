f57d8c04dd133e16cb8bfbd4b3dbc6302b7050ed
CMS <ayd07@naver.com>
2023-11-15 22:20:49 +0900

    feat: set hilt for app module

---
+++ app/src/main/java/com/sergio/dailyplan/App.kt
@@ -0,0 +1,7 @@
+package com.sergio.dailyplan
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application()
--- app/src/main/java/com/sergio/dailyplan/MainActivity.kt
+++ app/src/main/java/com/sergio/dailyplan/MainActivity.kt
@@ -10,18 +10,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.sergio.ui.theme.DailyPlanTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DailyPlanTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
 
-                }
             }
         }
     }
